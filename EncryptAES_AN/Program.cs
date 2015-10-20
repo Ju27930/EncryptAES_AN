a89ddbfe1f33e0334181552eb55a50e9914dae53
Ju27930 <techerjulien@yahoo.fr>
2015-10-20 14:19:05 +0200

    Création de la méthode de chiffrement

---
+++ EncryptAES_AN/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace EncryptAES_AN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EncryptString("Texte a chiffrer", "clé", "vecteur d'initialisation"));
+            Console.ReadLine();
+        }
+
+        private static string EncryptString(string clearText, string strKey, string strIv)
+        {
+
+            // Place le texte à chiffrer dans un tableau d'octets
+            byte[] plainText = Encoding.UTF8.GetBytes(clearText);
+
+            // Place la clé de chiffrement dans un tableau d'octets
+            byte[] key = Encoding.UTF8.GetBytes(strKey);
+
+            // Place le vecteur d'initialisation dans un tableau d'octets
+            byte[] iv = Encoding.UTF8.GetBytes(strIv);
+
+
+            RijndaelManaged rijndael = new RijndaelManaged();
+
+            // Définit le mode utilisé
+            rijndael.Mode = CipherMode.CBC;
+
+            // Crée le chiffreur AES - Rijndael
+            ICryptoTransform aesEncryptor = rijndael.CreateEncryptor(key, iv);
+
+            MemoryStream ms = new MemoryStream();
+
+            // Ecris les données chiffrées dans le MemoryStream
+            CryptoStream cs = new CryptoStream(ms, aesEncryptor, CryptoStreamMode.Write);
+            cs.Write(plainText, 0, plainText.Length);
+            cs.FlushFinalBlock();
+
+
+            // Place les données chiffrées dans un tableau d'octet
+            byte[] CipherBytes = ms.ToArray();
+
+
+            ms.Close();
+            cs.Close();
+
+            // Place les données chiffrées dans une chaine encodée en Base64
+            return Convert.ToBase64String(CipherBytes);
+
+
+        }
+
+    }
+}
